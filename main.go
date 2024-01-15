ecc0ad98cf23257c4d5c6a21c775f7e841756714
Marin Peric Radan <marin.peric@seekandhit.com>
2024-01-15 10:06:48 +0100

    Implemented a simple command line repl which will serve as a basis for the project

--- main.go
+++ main.go
@@ -1,9 +1,54 @@
 package main
+
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
+type cliCommand struct {
+		name string
+		description string
+		callback func() error
+	}
+	var commands = map[string]cliCommand{
+		"help": {
+			name: "help",
+			description: "Displays a help message",
+			callback: helpCommand,
+		},
+		"exit": {
+			name: "exit",
+			description: "Exits the pokedex",
+			callback: exitCommand,
+		},
+	}
+
+	func helpCommand() error {
+		fmt.Println("Welcome to the Pokedex!\n\nUsage:\n\nhelp: Displays a help message\nexit: Exit the Pokedex")
+		return nil
+	}
+
+	func exitCommand() error {
+		os.Exit(0)
+		return nil
+	}
 
 func main() {
-	fmt.Println("Hellooooo Faro!")
-}
+	scanner := bufio.NewScanner(os.Stdin)
 
+	for {
+		fmt.Print("pokedex> ")
+		scanner.Scan()
+		text := scanner.Text()
+
+		command, exists := commands[text]
+		if exists {
+			err := command.callback()
+			if err != nil {
+				fmt.Printf("Failed to execute command %s: %s", command.name, err)
+			}
+		} else {
+			fmt.Println("Your pokemon is >", text)
+		}
+	}
+}
